c4d052c3ff1e814f371a7b399f7fe363bbffe1db
john-rocky <john-rocky@hubot.com>
2021-06-21 12:33:01 +0900

    Initial Commit

---
+++ RealityKitSampler/RealityKitSamplerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealityKitSamplerApp.swift
+//  RealityKitSampler
+//
+//  Created by 間嶋大輔 on 2021/06/21.
+//
+
+import SwiftUI
+
+@main
+struct RealityKitSamplerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RealityKitSamplerTests/RealityKitSamplerTests.swift
@@ -0,0 +1,33 @@
+//
+//  RealityKitSamplerTests.swift
+//  RealityKitSamplerTests
+//
+//  Created by 間嶋大輔 on 2021/06/21.
+//
+
+import XCTest
+@testable import RealityKitSampler
+
+class RealityKitSamplerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
