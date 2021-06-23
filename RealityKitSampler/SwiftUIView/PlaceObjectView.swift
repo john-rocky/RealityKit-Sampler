77b9bc17c0c60979278338878ccdf955a4d6b057
john-rocky <john-rocky@hubot.com>
2021-06-23 13:18:24 +0900

    pickers

--- RealityKitSampler/SwiftUIView/PlaceObjectView.swift
+++ RealityKitSampler/SwiftUIView/PlaceObjectView.swift
@@ -10,21 +10,75 @@ import RealityKit
 
 struct PlaceObjectView: View {
     @State private var tappedLocation: CGPoint = .zero
-    
+    @State private var model = PlacingObjectModel()
+    @State private var meshMenuText = "Mesh:Box"
+    @State private var color = Color(.white)
+
     var body: some View {
-        ARContainerView(tappedLocation: $tappedLocation)
-            .edgesIgnoringSafeArea(.all)
-            .gesture(
-                DragGesture(minimumDistance: 0, coordinateSpace: .local).onEnded({ value in
-                    tappedLocation = value.location
-                }))
+        
+        ZStack {
+            ARContainerView(tappedLocation: $tappedLocation, model: $model)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                
+                Spacer()
+                
+                HStack {
+                    VStack(spacing: -20) {
+                        Text("Mesh")
+                            .font(.headline)
+                        Picker("", selection: $model.meshType) {
+                            Text(PlacingObjectModel.MeshType.box.rawValue).tag(PlacingObjectModel.MeshType.box)
+                            Text(PlacingObjectModel.MeshType.plane.rawValue).tag(PlacingObjectModel.MeshType.plane)
+                            Text(PlacingObjectModel.MeshType.sphere.rawValue).tag(PlacingObjectModel.MeshType.sphere)
+                        }
+                        .frame(width: 70)
+                        .clipped()
+                        
+                    }
+//                    Menu(meshMenuText) {
+//                        Button("Box",action:{
+//                            meshMenuText = "Mesh:Box"
+//                            model.meshType = .box
+//                        })
+//                        Button("Plane",action:{
+//                            meshMenuText = "Mesh:Plane"
+//                            model.meshType = .plane
+//                        })
+//                        Button("Sphere",action:{
+//                            meshMenuText = "Mesh:Sphere"
+//                            model.meshType = .sphere
+//                        })
+//                    }
+                    Menu("Material:") {
+                        Button("SimpleColor",action:{
+                            print(color)
+                        })
+                        Button("Image",action:{})
+                        Button("Video",action:{})
+                        Button("Occlusion",action:{})
+                    }
+                    ColorPicker("", selection: $color)
+                        .frame(width: 50, height: 50, alignment:.center)
+                        .onChange(of: color) { _color in
+                            model.color = UIColor(_color)
+                        }
+                    Menu("Physics") {
+                        Button("false",action:{})
+                        Button("true",action:{})
+                    }
+                }
+            }
+        }
     }
 }
 
 struct ARContainerView: View {
     @Binding var tappedLocation: CGPoint
+    @Binding var model:PlacingObjectModel
     var body: some View {
-        return ARViewContainer(tappedLocation: $tappedLocation)
+        return ARViewContainer(tappedLocation: $tappedLocation,model: $model)
             .edgesIgnoringSafeArea(.all)
     }
 }
@@ -32,19 +86,21 @@ struct ARContainerView: View {
 
 struct ARViewContainer: UIViewRepresentable {
     @Binding var tappedLocation: CGPoint
-    
+    @Binding var model:PlacingObjectModel
+
     func update(angles:simd_float3) {
         
     }
     
     func makeUIView(context: Context) -> PlacingObjectARView {
-        let arView = PlacingObjectARView(frame: .zero,settings: "")
+        let arView = PlacingObjectARView(frame: .zero,model: model)
         
         return arView
     }
     
     func updateUIView(_ uiView: PlacingObjectARView, context: Context) {
         uiView.tappedLocation = tappedLocation
+        uiView.model = model
     }
 }
 
--- RealityKitSampler/UIKitView/PlacingObjectARView.swift
+++ RealityKitSampler/UIKitView/PlacingObjectARView.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SwiftUI
 import RealityKit
 import ARKit
 
@@ -14,11 +15,17 @@ class PlacingObjectARView: ARView, ARSessionDelegate {
     var tappedLocation: CGPoint = CGPoint.zero {
         didSet {
             print(tappedLocation)
+            print(model.meshType)
         }
     }
     
-    init(frame: CGRect, settings: String) {
+     var model:PlacingObjectModel!
+    
+    init(frame: CGRect, model: PlacingObjectModel) {
         super.init(frame: frame)
+        self.model = model
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapped(sender:)))
+        self.addGestureRecognizer(tapGesture)
     }
 
     required init?(coder decoder: NSCoder) {
@@ -29,6 +36,39 @@ class PlacingObjectARView: ARView, ARSessionDelegate {
         fatalError("init(frame:) has not been implemented")
     }
 
+    @objc func tapped(sender: UITapGestureRecognizer){
+        let location = sender.location(in: self)
+        let results = self.raycast(from: location, allowing: .estimatedPlane, alignment: .any)
+        if let firstResult = results.first {
+            let anchor = ARAnchor(name: "Anchor for object placement", transform: firstResult.worldTransform)
+            self.session.add(anchor: anchor)
+            let anchorEntity = AnchorEntity(anchor: anchor)
+            let modelEntity = ModelEntity(mesh: generateMesh())
+            modelEntity.model?.materials = [generateMaterial()]
+            anchorEntity.addChild(modelEntity)
+            self.scene.addAnchor(anchorEntity)
+        }
+    }
+    
+    func generateMesh() -> MeshResource {
+        var mesh:MeshResource
+        switch model.meshType {
+        case .box:
+            mesh = .generateBox(size: 0.05)
+        case .plane:
+            mesh = .generatePlane(width: 0.05, depth: 0.05)
+        case .sphere:
+            mesh = .generateSphere(radius: 0.025)
+        }
+        return mesh
+    }
+    
+    func generateMaterial() -> Material {
+        var material:Material
+        material = SimpleMaterial(color: model.color, isMetallic: true)
+        return material
+    }
+    
     func setup() {
         
     }
